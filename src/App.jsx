145fd54a0dca8bf630c87c3c28fd8cdca80d6f4c
pietrobertino <pietrobertino20@gmail.com>
2026-03-16 12:42:59 +0100

    complete selection feature

--- src/App.jsx
+++ src/App.jsx
@@ -11,23 +11,46 @@ const movies = [
 
 function App() {
 
+  const [movieList, setMovieList] = useState(movies);
+  const [filteredList, setFilteredList] = useState([]);
+  const [selectedGenre, setSelectedGenre] = useState("");
+
+
+  useEffect(() => {
+    //dobbiamo fare in modo che a seconda del filtro scelto, la filtered list cambi
+    const newList = movieList.filter((movie) => movie.genre.toLowerCase().includes(selectedGenre))
+    setFilteredList(newList)
+
+  }, [movieList, selectedGenre])
+
   return (
     <>
       <div className="container">
-        <h1 className=" text-center">Movie list</h1>
+
+        <h1 className=" text-center py-3">Movie list</h1>
+
         <div className="mb-3">
           <label htmlFor="genres" className="form-label">Genre</label>
           <select
             className="form-select form-select-lg"
             name="genres"
             id="genres"
+            onChange={(e) => setSelectedGenre(e.target.value)}
           >
+            {/* dobbiamo capire come fare in modo che le opzioni siano collegate con selectedGenre */}
+            <option value="">Tutti i film</option>
             <option value="fantascienza">Fantascienza</option>
             <option value="thriller">Thriller</option>
             <option value="romantico">Romantico</option>
             <option value="azione">Azione</option>
           </select>
         </div>
+
+        <ul>
+          {filteredList.map((movie) => (
+            <li key={movie.title}>{movie.title}</li>
+          ))}
+        </ul>
 
       </div>
 
